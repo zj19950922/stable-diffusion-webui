ecd1dfe29833fc4db96b702032c24639576abb4c
zj <zhujun19950922@163.com>
2024-04-18 16:01:20 +0800

    修改配置

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--medvram --opt-split-attention --enable-insecure-extension-access --xformers --no-gradio-queue --no-half
 
 call webui.bat
